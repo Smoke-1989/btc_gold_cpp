079a84bac2d3044da29ba30cc0d3a0296bf53f6c
Smoke <1989.smoke@gmail.com>
2025-12-30 14:21:23 -0300

    Fix: Update header with new check_and_save signature

--- include/worker.h
+++ include/worker.h
@@ -1,26 +1,22 @@
 #pragma once
 
 #include "types.h"
+#include "config.h"
 #include "database.h"
 #include "hash160.h"
 #include "secp256k1_wrapper.h"
-#include <thread>
-#include <atomic>
-#include <vector>
-#include <cstdint>
 #include <memory>
 
 namespace btc_gold {
 
+struct Stats {
+    std::atomic<uint64_t> total_keys{0};
+    std::atomic<uint64_t> found_count{0};
+    std::atomic<bool> should_stop{false};
+};
+
 class Worker {
 public:
-    struct Stats {
-        std::atomic<uint64_t> total_keys{0};
-        std::atomic<uint64_t> found_count{0};
-        std::atomic<bool> should_stop{false};
-        uint64_t start_time = 0;
-    };
-    
     Worker(
         int worker_id,
         const Config& config,
@@ -29,21 +25,25 @@ public:
     );
     
     void run();
-    
+
 private:
+    void run_linear_mode();
+    void run_random_mode();
+    void run_geometric_mode();
+    
+    // Updated signature: added 'bool compressed'
+    void check_and_save(const PrivateKey& privkey, const Hash160& hash160, bool compressed);
+
     int worker_id_;
     const Config& config_;
     const Database& database_;
     Stats& stats_;
     
+    // Each worker has its own hash engine to be thread-safe
     std::unique_ptr<Hash160Engine> hash_engine_;
-    const Secp256k1& secp256k1_ = Secp256k1::instance();
     
-    void run_linear_mode();
-    void run_random_mode();
-    void run_geometric_mode();
-    
-    void check_and_save(const PrivateKey& privkey, const Hash160& hash160);
+    // Singleton wrapper for secp256k1 context
+    Secp256k1& secp256k1_ = Secp256k1::instance();
 };
 
 }  // namespace btc_gold
