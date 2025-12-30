793ede4bc6e989f923785c1f42b9de0eaf74eb3e
Smoke <1989.smoke@gmail.com>
2025-12-30 13:30:36 -0300

    Fix: Implement complete CLI argument parser

--- src/config.cpp
+++ src/config.cpp
@@ -2,6 +2,7 @@
 #include "logger.h"
 #include <iostream>
 #include <cstring>
+#include <stdexcept>
 
 namespace btc_gold {
 
@@ -9,8 +10,70 @@ Config ConfigParser::parse_cli(int argc, char** argv) {
     Config config;
     
     for (int i = 1; i < argc; ++i) {
-        if (std::strcmp(argv[i], "--help") == 0) {
+        std::string arg = argv[i];
+        
+        if (arg == "--help" || arg == "-h") {
             print_help();
+            std::exit(0);
+        }
+        else if (arg == "--threads" || arg == "-t") {
+            if (i + 1 < argc) {
+                config.threads = std::stoi(argv[++i]);
+            }
+        }
+        else if (arg == "--mode" || arg == "-m") {
+            if (i + 1 < argc) {
+                std::string mode = argv[++i];
+                if (mode == "linear" || mode == "1") {
+                    config.mode = Config::Mode::LINEAR;
+                } else if (mode == "random" || mode == "2") {
+                    config.mode = Config::Mode::RANDOM;
+                } else if (mode == "geometric" || mode == "3") {
+                    config.mode = Config::Mode::GEOMETRIC;
+                }
+            }
+        }
+        else if (arg == "--scan-mode" || arg == "-s") {
+            if (i + 1 < argc) {
+                int mode = std::stoi(argv[++i]);
+                config.scan_mode = static_cast<Config::ScanMode>(mode);
+            }
+        }
+        else if (arg == "--database" || arg == "-d") {
+            if (i + 1 < argc) {
+                config.database_file = argv[++i];
+            }
+        }
+        else if (arg == "--start") {
+            if (i + 1 < argc) {
+                std::string val = argv[++i];
+                if (val.substr(0, 2) == "0x" || val.substr(0, 2) == "0X") {
+                    config.start_value = std::stoull(val, nullptr, 16);
+                } else {
+                    config.start_value = std::stoull(val);
+                }
+            }
+        }
+        else if (arg == "--end") {
+            if (i + 1 < argc) {
+                std::string val = argv[++i];
+                if (val.substr(0, 2) == "0x" || val.substr(0, 2) == "0X") {
+                    config.end_value = std::stoull(val, nullptr, 16);
+                } else {
+                    config.end_value = std::stoull(val);
+                }
+            }
+        }
+        else if (arg == "--multiplier") {
+            if (i + 1 < argc) {
+                config.multiplier = std::stod(argv[++i]);
+            }
+        }
+        else if (arg == "--input-type") {
+            if (i + 1 < argc) {
+                int type = std::stoi(argv[++i]);
+                config.input_type = static_cast<Config::InputType>(type);
+            }
         }
     }
     
@@ -53,6 +116,16 @@ void ConfigParser::print_help() {
     std::cout << "BTC GOLD C++ - Usage\n";
     std::cout << "  btc_gold [options]\n";
     std::cout << "  btc_gold --help\n";
+    std::cout << "\nOptions:\n";
+    std::cout << "  --threads, -t <N>          Number of threads (default: auto)\n";
+    std::cout << "  --mode, -m <mode>          Mode: linear, random, geometric (default: random)\n";
+    std::cout << "  --scan-mode, -s <N>        Scan: 1=compressed, 2=uncompressed, 3=both (default: 3)\n";
+    std::cout << "  --database, -d <file>      Database file (default: alvos.txt)\n";
+    std::cout << "  --start <value>            Start value (decimal or hex with 0x)\n";
+    std::cout << "  --end <value>              End value (decimal or hex with 0x)\n";
+    std::cout << "  --multiplier <value>       Multiplier for geometric mode (default: 1.5)\n";
+    std::cout << "  --input-type <N>           Input: 1=address, 2=hash160, 3=pubkey (default: 1)\n";
+    std::cout << "  --help, -h                 Show this help\n";
 }
 
 }  // namespace btc_gold
